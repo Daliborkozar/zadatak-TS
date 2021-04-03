fd4215fb1fac540136572ad9b0e112cfd014dbb1
dalibor kozar <kozardalibor@gmail.com>
2021-04-03 13:27:56 +0200

    nav hamburger menu

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,66 +1,81 @@
 import React, { useState } from "react";
-import { FaRegUserCircle,FaSearch, FaBars } from "react-icons/fa";
+import { FaRegUserCircle, FaSearch, FaBars,FaTimes } from "react-icons/fa";
 import { useHistory, useLocation } from "react-router";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const history = useHistory();
   const location = useLocation();
-  const [show,setShow] = useState(false)
-  const [search, setSearch] = useState('')
+  const [show, setShow] = useState(false);
+  const [search, setSearch] = useState("");
+  const [menuShow, setMenuShow] = useState(false)
 
   const submitHandler = (e) => {
     e.preventDefault();
-    history.push(`/search/${search}`)
-    setSearch('')
+    history.push(`/search/${search}`);
+    setSearch("");
+    setShow(!show)
   };
-  
+
   const onSearchChange = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   return (
-    <header className="navbar">
+    <nav className="navbar">
       <div className="navbar__searchbox">
-        <form className='navbar-form' onSubmit={submitHandler}>
+        <form className="navbar-form" onSubmit={submitHandler}>
           <input
             type="text"
             placeholder="Search recipe.."
             value={search}
             onChange={onSearchChange}
           />
-          <span className='navbar-form__icon'><FaSearch size={20}/></span>
+          <span className="navbar-form__icon">
+            <FaSearch size={20} />
+          </span>
         </form>
       </div>
       <div className="navbar__right">
-        <div className='hamburger'>
-          <FaBars />
+        <div className="tooltip">
+          {show ? (
+            <form className="tooltip-form">
+              <input
+                className="tooltip-input"
+                type="text"
+                placeholder="Username"
+              />
+              <input
+                className="tooltip-input"
+                type="text"
+                placeholder="Password"
+              />
+              <button className="tooltip-button">Login</button>
+            </form>
+          ) : null}
+
+          <FaRegUserCircle size={40} onClick={() => setShow((prev) => !prev)} />
         </div>
-       
-        <div className='tooltip'>
-        {show ? (<form className='tooltip-form' >
-            <input className='tooltip-input' type='text' placeholder="Username"/>
-            <input className='tooltip-input' type='text' placeholder='Password'/>
-            <button className='tooltip-button'>Login</button>
-          </form>) : null}
-          
-        <FaRegUserCircle size={40} onClick={() => setShow(prev => !prev)} />
+        <div className="hamburger" onClick={()=> setMenuShow(prev => !prev)}>
+          {menuShow ? <FaTimes size={30}/> : <FaBars size={30} />}
         </div>
-        <ul className="navbar-list">
-          {location.pathname === "/" ? null : (
+        <div className={`navbar-wrapper ${menuShow ? 'active' : null }`}>
+          <ul className="navbar-list" onClick={()=> setMenuShow(prev => !prev)}>
+            {location.pathname === "/" ? null : (
+              <Link to="/">
+                <li className="navbar-list__item">Home</li>
+              </Link>
+            )}
             <Link to="/">
-              <li className="navbar-list__item">Home</li>
+              <li className="navbar-list__item">About us</li>
             </Link>
-          )}
-          <Link to="/">
-            <li className="navbar-list__item">About us</li>
-          </Link>
-          <Link to="/">
-            <li className="navbar-list__item">Contact</li>
-          </Link>
-        </ul>
+            <Link to="/">
+              <li className="navbar-list__item">Contact</li>
+            </Link>
+          </ul>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
